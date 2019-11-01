faca8aa992a99b6fb08307349308b10db4cbc952
Julie Davison <davison.jules@gmail.com>
2019-10-31 21:31:24 -0700

    fixing build

---
+++ client/build/precache-manifest.46c62d7cc7ccebc623d3a31a20ebe6cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f305be1e063a6b9b71f7cc706916e09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b143390f64c6903a5038",
+    "url": "/static/css/main.8d2bd942.chunk.css"
+  },
+  {
+    "revision": "80d5acfb6b2672a354af",
+    "url": "/static/js/2.b730665c.chunk.js"
+  },
+  {
+    "revision": "b143390f64c6903a5038",
+    "url": "/static/js/main.002ac60e.chunk.js"
+  },
+  {
+    "revision": "3c6a2477a7d0b6de4ed7",
+    "url": "/static/js/runtime-main.25c6d7c9.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
